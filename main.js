6ab5bdc5947134ce1e5186f8804685a5780c1bc3
Frankmona <frankoguche1@gmail.com>
2023-03-29 17:19:16 +0100

    learning

---
+++ main.js
@@ -0,0 +1,35 @@
+// console.log('Hello mona')
+// console.log('Healing takes time')
+// console.log('You can make it bro!')
+// //console.log('WHY? Mercy')
+
+//variable uses CONST AND LET //Const is used wen the value assigned is fixed a nd let is used when u would like to change the values.
+
+// let age = 35;
+// age = 56; //new result become 56
+// console.log(age);
+
+// const salary = 1000;
+// console.log(salary);
+
+// // non-primitive datatype
+// const person = {
+//   firstname: "frank",
+//   lastname: "mona",
+//   age: 400,
+// };
+// console.log(person.firstname);
+
+// using array
+// const oddnumber = [1, 2, 3, 4, 5, 9];
+// console.log(oddnumber[0]);
+
+// Operators
+
+// Assignment operator
+// let x = 40;
+
+// Arithemetic operators
+let x = 10;
+y = 5;
+console.log(x + y);
