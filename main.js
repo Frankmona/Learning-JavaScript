f0ff64e1446e0e7ff6a1e9bdefbda4da1c3907f2
Frankmona <frankoguche1@gmail.com>
2023-03-30 17:36:42 +0100

    learning

--- main.js
+++ main.js
@@ -27,9 +27,51 @@
 // Operators
 
 // Assignment operator
+
 // let x = 40;
 
 // Arithemetic operators
+
 let x = 10;
 y = 5;
-console.log(x + y);
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x % y);
+// console.log(++x);
+// console.log(--x);
+
+// comparison operator
+
+// console.log(x == y);
+// console.log(x > y);
+// console.log(x === y);
+// console.log(x !== y);
+
+// logical operators
+// AND symbol
+// const isValid = x > 8 && 8 > y;
+// console.log(isValid);
+
+// OR symbol
+// const isValid = x < 8 || 8 > y;
+// console.log(isValid);
+
+// NOT symbol
+// const isValid = x > 8 || 8 > y;
+// console.log(!isValid);
+
+// string operators
+
+// console.log("Frank " + "Mona");
+
+// const isEven = 10 % 2 === 0 ? true : false;
+// (means if 10/2 and the remainder is equeal to zero print true else print false)
+// console.log(isEven);
+
+// Type conversion
+// Implicit conversion which is done by javascripts
+// explicit conversion which is done manually
+
+// console.log(12 + "3");
+
+console.log("5" + "3");
